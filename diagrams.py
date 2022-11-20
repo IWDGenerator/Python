56e2287c5d16eba219440708b85dee855bb618f1
IWDGenerator <118289186+IWDGenerator@users.noreply.github.com>
2022-11-20 11:06:48 +0100

    Add files via upload

---
+++ diagrams.py
@@ -0,0 +1,15 @@
+import graphviz
+import json
+import sys
+
+dot = graphviz.Digraph('pipelines-diagram', comment='Project Diagram')
+dot.attr('graph', splines='ortho')
+dot.attr('node', fontname='Arial')
+
+config = json.load(open(sys.argv[1],'r'))
+for i in config['result']:
+    dot.node(i['id'],i['pipeline'], shape='box')
+    if 'connection' in i.keys():
+        dot.edge(i['connection'],i['id'])
+
+dot.render(outfile=f'/opt/snaplogic/diagrams/{sys.argv[2]}.png')
